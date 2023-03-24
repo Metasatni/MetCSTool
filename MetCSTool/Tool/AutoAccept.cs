5ee01a313a00d0d7a4cfa62a91962ab0d085a45c
pawelkobus920@gmail.com <pawelkobus920@gmail.com>
2023-03-24 19:49:13 +0100

    autoaccept c;

--- MetCSTool/Tool/AutoAccept.cs
+++ MetCSTool/Tool/AutoAccept.cs
@@ -30,36 +30,36 @@ namespace MetCSTool.Tool
             while (this.Enabled)
             {
                 await Task.Run(() => Thread.Sleep(LatencyInMs));
-                var screen = ScreenFunc.TakeScreenshot(ResolutionWidth, ResolutionHeight, 100, 200);
+                var screen = ScreenFunc.TakeScreenshot(ResolutionWidth - (ResolutionWidth / 10), ResolutionHeight - (ResolutionHeight / 10), 100, 200);
                 screen = ConvertToFormat(screen, PixelFormat.Format24bppRgb);
                 TemplateMatch[] matches = new ExhaustiveTemplateMatching().ProcessImage(
                 new ResizeBilinear(screen.Width, screen.Height).Apply(screen),
                 new ResizeBilinear(patternImage.Width, patternImage.Height).Apply(patternImage)
                 );
-                    int indexMaxSim = -1;
-                    float maxSim = 0;
-                    int indCounter = 0;
-                    if (matches.Length > 0)
+                int indexMaxSim = -1;
+                float maxSim = 0;
+                int indCounter = 0;
+                if (matches.Length > 0)
+                {
+                    foreach (var item in matches)
                     {
-                        foreach (var item in matches)
+                        if (item.Similarity > 0.90)
                         {
-                            if (item.Similarity > 0.90)
+                            if (item.Similarity > maxSim)
                             {
-                                if (item.Similarity > maxSim)
-                                {
-                                    indexMaxSim = indCounter;
-                                    maxSim = item.Similarity;
-                                }
-                                indCounter++;
+                                indexMaxSim = indCounter;
+                                maxSim = item.Similarity;
                             }
+                            indCounter++;
                         }
-                        if (indexMaxSim == -1) return;
-                        Cursor.Position = new Point( Convert.ToInt32(this.ResolutionWidth * 0.6) , Convert.ToInt32(this.ResolutionHeight * 0.5));
-                        MouseInput.MouseClick();
                     }
-                screen.Dispose();
+                    if (indexMaxSim == -1) return;
+                    Cursor.Position = new Point(Convert.ToInt32(this.ResolutionWidth * 0.6), Convert.ToInt32(this.ResolutionHeight * 0.5));
+                    //MouseInput.MouseClick();
                 }
-    
+                screen.Dispose();
             }
+
         }
     }
+}
